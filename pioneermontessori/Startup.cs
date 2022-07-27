45a00c34543b42bcbeb631854ffa5a90a1adb9f5
kashyapamit856 <kashyap.amit856@gmail.com>
2022-07-28 00:57:34 +0530

    update slider

--- pioneermontessori/Startup.cs
+++ pioneermontessori/Startup.cs
@@ -46,7 +46,7 @@ namespace pioneermontessori
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Web}/{action=Index}/{id?}");
             });
         }
     }

763b8c8fc8e0d7da3edfd6c5ecdfb4c766fd6650
kashyapamit856 <kashyap.amit856@gmail.com>
2022-09-07 09:24:42 +0530

    update 9-7-2022

--- pioneermontessori/Controllers/WebController.cs
+++ pioneermontessori/Controllers/WebController.cs
@@ -12,5 +12,89 @@ namespace pioneermontessori.Controllers
         {
             return View();
         }
+        public IActionResult VisionMission()
+        {
+            return View();
+        }
+
+        public IActionResult FoundersMessage()
+        {
+            return View();
+        }
+
+        public IActionResult ManagersMessage()
+        {
+            return View();
+        }
+
+        public IActionResult AdmissionProcedure()
+        {
+            return View();
+        }
+
+        public IActionResult FreeStructre()
+        {
+            return View();
+        }
+      
+        public IActionResult Downloads()
+        {
+            return View();
+        }
+
+        public IActionResult FeeStructure()
+        {
+            return View();
+        }
+
+        public IActionResult Uniform()
+        {
+            return View();
+        }
+
+        public IActionResult CBSEBoard()
+        {
+            return View();
+        }
+
+        public IActionResult ICSEBoard()
+        {
+            return View();
+        }
+        public IActionResult UPBoard()
+        {
+            return View();
+        }
+
+        public IActionResult DegreeCollege()
+        {
+            return View();
+        }
+
+        public IActionResult Infrastructure()
+        {
+            return View();
+        }
+
+        public IActionResult PhotoGallery()
+        {
+            return View();
+        }
+
+        public IActionResult VideoGallery()
+        {
+            return View();
+        }
+
+        public IActionResult MediaGallery()
+        {
+            return View();
+        }
+
+        public IActionResult ContactUs()
+        {
+            return View();
+        }
+
     }
 }
